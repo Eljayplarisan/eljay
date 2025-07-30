b96911c170053af013cfdd9501786b7c8cb252cd
canazareserichjane@gmail.com <canazareserichjane@gmail.com>
2025-07-29 23:21:04 -0700

    new

---
+++ src/repo/pkg1/repo.java
@@ -0,0 +1,33 @@
+package repo.pkg1;
+
+import java.util.Scanner;
+
+public class repo {
+    
+    public static void main(String[] args) {
+        Scanner lp = new Scanner(System.in);
+        
+        
+        
+        System.out.print("Enter employee name: ");
+        String name = lp.nextLine();
+        
+        System.out.print("Enter hourly rate: ");
+        float rate = lp.nextInt();
+        
+        System.out.print("Enter total hours worked this week: ");
+        float week = lp.nextInt();
+        
+        float Wage = rate*week;
+        float SSS = Wage * 0.1f;
+         
+        System.out.print("\n--------Wage Summery--------");
+        System.out.print("\nEmployee: " +name );
+        System.out.print("\nHourly Rate: " +rate);
+        System.out.printf("\nGross weekly Wage: %.2f\n", Wage);
+        System.out.printf("\nSSS contribution (10%%): %.2f\n", SSS);
+        
+         
+    }
+    
+}
